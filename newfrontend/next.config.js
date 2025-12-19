c2241fe0313a57cf3386d7925fe829f1b9fa8214
Ahmad Wail <ahmad200wail@gmail.com>
2025-12-20 01:58:22 +0200

    deployment

--- newfrontend/next.config.js
+++ newfrontend/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
       },
     ],
   },
